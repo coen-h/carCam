e0034c2a320bfeb8b33e659c7c321ed46625b4c0
coen-h <105825347+coen-h@users.noreply.github.com>
2025-06-19 09:34:19 +1200

    initial

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { PrismaClient } from './generated/prisma'
+
+const prisma = new PrismaClient()
+
+export default function Home() {
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  );
+}
