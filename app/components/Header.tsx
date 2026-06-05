fc28c431c8ba28245e5d9f077a859c572b14d253
coen-h <105825347+coen-h@users.noreply.github.com>
2026-06-06 00:13:23 +1200

    change to btn

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,35 +14,14 @@ export default function Header() {
     <div className="navbar min-h-0 flex justify-between z-50">
       <button className="btn btn-soft text-lg">carCam</button>
       <div className="flex items-center gap-2">
-        {/* <div className="dropdown"> */}
           <label className="swap swap-rotate btn btn-soft btn-square">
             <input type="checkbox" className="theme-controller" value="light" />
             <Sun className="swap-on" width={24} />
             <Moon className="swap-off" width={24} />
           </label>
-          {/* <ul tabIndex="-1" className="dropdown-content bg-base-300 mt-1 rounded-box z-1 w-40 p-2 shadow-2xl">
-            <li>
-              <input type="radio" name="theme-dropdown" className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start text-base-content" aria-label="Default" value="default" />
-            </li>
-            <li>
-              <input type="radio" name="theme-dropdown" className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start text-base-content" aria-label="Dracula" value="dracula" />
-            </li>
-            <li>
-              <input type="radio" name="theme-dropdown" className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start text-base-content" aria-label="Forest" value="forest" />
-            </li>
-            <li>
-              <input type="radio" name="theme-dropdown" className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start text-base-content" aria-label="Dim" value="dim" />
-            </li>
-            <li>
-              <input type="radio" name="theme-dropdown" className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start text-base-content" aria-label="Business" value="business" />
-            </li>
-            <li>
-              <input type="radio" name="theme-dropdown" className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start text-base-content" aria-label="Pastel" value="pastel" />
-            </li>
-          </ul> */}
-        {/* </div> */}
+          
         {/* <button data-tip="Logout" className='tooltip tooltip-bottom font-normal btn btn-square btn-soft rounded-box' onClick={() => void signOut()}><LogOut /></button> */}
-        <div className="menu menu-xs p-1 menu-horizontal bg-base-content/8 text-base-content rounded-box">
+        <div className="menu menu-xs p-1 menu-horizontal btn btn-soft rounded-box">
           <li>
             <Link href='/' className="tooltip tooltip-bottom text-base-content" data-tip="Home">
               <Home width={24} />
