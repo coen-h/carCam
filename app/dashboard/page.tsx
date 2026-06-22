000740ecba190ad6e773f204da35dcad255eec85
Coen <105825347+coen-h@users.noreply.github.com>
2026-06-22 14:19:58 +1200

    full dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -27,16 +27,15 @@ export default function Dashboard() {
     <div className='w-screen h-screen bg-base-100'>
       <Background />
       <Header setIsDarkCom={setIsdarkCom} />
-      <div className="flex flex-col gap-4 xl:container mx-auto p-4">
-        <div className='flex justify-center items-center gap-2'>
-          <div className='skeleton aspect-video w-full flex-1 relative shadow-sm'>
+      <div className="flex justify-center gap-2 xl:container mx-auto p-4">
+          <div className='skeleton aspect-video w-full shrink-0 relative shadow-sm'>
             {/* <img className='rounded w-full h-full' src="http://192.168.0.137:5000/video_feed" /> */}
             <img className='rounded w-full h-full' src="https://tkhsecurity.com/wp-content/uploads/2025/04/box-5-1920x1080.png" />
             <div className="size-2 animate-pulse bg-red-300 top-1 right-1 rounded-full absolute" />
             <div className="top-0 left-0 absolute bg-base-100/60 backdrop-blur text-base-content rounded-tl rounded-br p-1">Student Parking Entrance</div>
           </div>
-          <div className='flex flex-col items-stretch gap-2 w-100 max-2xl:w-80'>
-            <div className='w-full aspect-video flex items-center justify-center'>
+          <div className='flex flex-col gap-2 w-100 max-2xl:w-80 self-stretch'>
+            <div className='w-full shrink-0 aspect-video flex items-center justify-center'>
               {logs ? logs?.slice(-1).map((log) =>
                 <div key={log.carPlate} className="bg-base-300 rounded">
                   <div className="skeleton w-full h-full rounded-t">
@@ -44,7 +43,7 @@ export default function Dashboard() {
                     <img className='object-cover rounded-t w-full h-full' src="https://tkhsecurity.com/wp-content/uploads/2025/04/box-5-1920x1080.png" />
                   </div>
                   <div className="flex justify-between text-base-content items-center p-1">
-                  < h3>Plate: {log.carPlate}</h3>
+                  <h3>Plate: {log.carPlate}</h3>
                     <p className='text-sm'>{new Date(log._creationTime).toLocaleTimeString()}</p>
                   </div>
                 </div>
@@ -52,7 +51,7 @@ export default function Dashboard() {
                 <div className="skeleton bg-base-300 w-full h-full rounded"></div>
               )}
             </div>
-            <div className="w-full h-full list gap-0.5 text-base-content shadow-sm bg-base-200 rounded-box p-1">
+            <div className="flex-1 min-h-0 w-full list gap-0.5 text-base-content shadow-sm bg-base-200 rounded-box p-1 overflow-y-auto">
               <p className='p-2 text-lg opacity-60 tracking-wide'>Latest Logs</p>  
               {logs ? logs?.slice(-5).reverse().map((log) =>
                 <button onClick={() => { (document.getElementById('my_modal_1') as HTMLDialogElement).showModal(); setSelected(log)}} key={log._id} className="btn btn-xl list-row items-center bg-base-300 flex justify-between">
@@ -64,7 +63,6 @@ export default function Dashboard() {
               )}
             </div>
           </div>
-        </div>
         
       </div>
       <dialog id="my_modal_1" className="modal text-base-content">
