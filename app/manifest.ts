39820b681c1b478fc391c8f9c23107bfc018bc76
Coen <105825347+coen-h@users.noreply.github.com>
2026-06-29 14:28:25 +1200

    add logos

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,9 +2,9 @@ import { MetadataRoute } from 'next';
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Student Parking Dashboard',
-    short_name: 'Parking',
-    description: 'Student parking entrance logs and registration',
+    name: 'carCam - Parking Surveillance',
+    short_name: 'carCam',
+    description: 'carCam is the solution for parking surveillance',
     start_url: '/',
     display: 'standalone',
     background_color: '#1d232a', 
